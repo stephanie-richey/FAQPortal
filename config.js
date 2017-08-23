2f2a9daf7c9c9a69a0b9d5b11505d735d47c5206
stephanie-richey <31261505+stephanie-richey@users.noreply.github.com>
2017-08-23 11:32:14 +1000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    API_AI_CLIENT_ACCESS_TOKEN: '8068c8ebbdff4136bf4d0f01c522be91',
+    SERVER_URL: "",
+    MAIL_CHIMP_ACCESS_TOKEN: '4642357b7968ff629cd5affd4b920f91-us16',
+    APIAI_SESSION_ID ='sessionidtest'
+};
